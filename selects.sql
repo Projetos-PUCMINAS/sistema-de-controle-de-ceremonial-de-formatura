7f0be4c0c09e790c6c7659f46f4cec1814c972b9
Bernardo Alvim <83983141+alvimdev@users.noreply.github.com>
2024-11-28 20:26:08 -0300

    Update selects.sql

--- selects.sql
+++ selects.sql
@@ -1,19 +1,19 @@
---1. Listar os nomes dos profissionais e o total de horas que trabalharam em eventos, ordenados pelo maior total de horas.
+-- 1. Exibir os tipos de aperitivos e a média de preço por pessoa para cada tipo.
 
---2. Exibir os nomes dos buffets e o número total de cardápios que eles possuem, ordenados do maior para o menor.
+-- 2. Exibir os tipos de aperitivos e o número total de cardápios em que cada tipo aparece.
 
---3. Listar os nomes dos eventos e a soma total dos preços dos aperitivos dos cardápios associados a esses eventos
+-- 3. Exibir os nomes dos buffets e o número total de cardápios que eles possuem, ordenados do maior para o menor.
 
---4. Exibir os nomes dos profissionais que trabalharam em mais de 3 eventos.
+-- 4. Listar os nomes dos profissionais e o total de horas que trabalharam em eventos, ordenados pelo maior total de horas.
 
---5. Listar os nomes e CNPJs dos buffets que possuem cardápios associados a eventos realizados no mês de novembro.
+-- 5. Listar os nomes dos eventos e a soma total dos preços dos aperitivos dos cardápios associados a esses eventos.
 
---6. Exibir os tipos de aperitivos e a média de preço por pessoa para cada tipo.
+-- 6. Exibir os nomes dos eventos e os profissionais que trabalharam neles por mais de 10 horas.
 
---7. Mostrar os buffets que possuem uma parceria maior que 10 anos, ordenados pelo tempo de parceria.
+-- 7. Exibir os nomes dos profissionais que trabalharam em mais de 3 eventos.
 
---8. Listar os eventos com o nome do chefe da turma e o valor total do pagamento por evento.
+-- 8. Listar os nomes e CNPJs dos buffets que possuem cardápios associados a eventos realizados no mês de novembro.
 
---9. Exibir os nomes dos eventos e os profissionais que trabalharam neles por mais de 10 horas.
+-- 9. Mostrar os buffets que possuem uma parceria maior que 10 anos, ordenados pelo tempo de parceria.
 
---10. Exibir os tipos de aperitivos e o número total de cardápios em que cada tipo aparece.
+-- 10. Listar os eventos com o nome do chefe da turma e o valor total do pagamento por evento.
